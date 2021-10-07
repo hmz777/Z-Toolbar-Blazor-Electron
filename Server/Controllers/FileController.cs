51d7beb1e939b887f1e83b2f58ef63d411583f25
HMZ <hamzi1995@gmail.com>
2021-10-07 16:58:51 +0300

    Add file controller for handling file specific tasks like fetching the icons and saving them.

---
+++ Server/Controllers/FileController.cs
@@ -0,0 +1,74 @@
+﻿using BlazorElectronToolbar.Shared;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorElectronToolbar.Server.Controllers
+{
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public FileController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("CreateFileIcon")]
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public IActionResult CreateFileIcon([FromBody] FileModel fileModel)
+        {
+            try
+            {
+                var iconPath = System.IO.Path.Combine(webHostEnvironment.ContentRootPath, "..", "Client", "wwwroot", "img", fileModel.FileId + ".png");
+
+                if (!System.IO.File.Exists(iconPath))
+                {
+                    var fileIcon = Icon.ExtractAssociatedIcon(fileModel.Path);
+
+                    using (var fileStream = new FileStream(iconPath, FileMode.Create))
+                    {
+                        fileIcon.Save(fileStream);
+                        fileStream.Flush();
+                    }
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [Route("RemoveIcon")]
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public IActionResult RemoveIcon([FromBody] string FileId)
+        {
+            try
+            {
+                var iconPath = System.IO.Path.Combine(webHostEnvironment.ContentRootPath, "..", "Client", "wwwroot", "img", FileId + ".png");
+
+                if (System.IO.File.Exists(iconPath))
+                {
+                    System.IO.File.Delete(iconPath);
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
