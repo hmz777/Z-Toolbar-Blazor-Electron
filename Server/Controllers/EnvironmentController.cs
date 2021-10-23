ce49353811c630791472e4fa4d58e0983ef4a167
HMZ <hamzi1995@gmail.com>
2021-10-23 04:23:32 +0300

    To do: Subscribe to the colors changed system event and change color based on it.

--- Server/Controllers/EnvironmentController.cs
+++ Server/Controllers/EnvironmentController.cs
@@ -23,6 +23,8 @@ namespace BlazorElectronToolbar.Server.Controllers
             var uISettings = new UISettings();
             var color = uISettings.GetColorValue(UIColorType.Accent);
 
+            //Subscribe to the colors changed event and change color based on it.
+
             return Ok(new { R = color.R, G = color.G, B = color.B, A = color.A });
         }
 
